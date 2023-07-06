780eab59f85a44c5dcc179dd246dd7a52790939e
Guillaume AMAT <guillaume.amat@backmarket.com>
2023-07-06 14:55:54 +0200

    doc: quote errors in comments

--- src/runtime/nitroPlugin.ts
+++ src/runtime/nitroPlugin.ts
@@ -1,9 +1,12 @@
 import type { NitroApp } from "nitropack";
 
+// Cannot find module '#nitro' or its corresponding type declarations.ts(2307)
 import { defineNitroPlugin } from "#nitro";
 
 export default defineNitroPlugin((nitroApp: NitroApp) => {
+  // Argument of type '"render:html"' is not assignable to parameter of type 'HookKeys<NitroRuntimeHooks>'.ts(2345)
   nitroApp.hooks.hook("render:html", (html) => {
+    // Property 'head' does not exist on type 'Partial<RenderResponse>'.ts(2339)
     html.head.push("<!-- Something -->");
   });
 });
