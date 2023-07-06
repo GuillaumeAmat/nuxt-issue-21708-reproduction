64d779288a298d7e0a7c0dd6b31703535626db80
Guillaume AMAT <guillaume.amat@backmarket.com>
2023-07-06 15:27:41 +0200

    fix: use #imports instead of #nitro

--- src/runtime/nitroPlugin.ts
+++ src/runtime/nitroPlugin.ts
@@ -1,7 +1,7 @@
 import type { NitroApp } from "nitropack";
 
-// Cannot find module '#nitro' or its corresponding type declarations.ts(2307)
-import { defineNitroPlugin } from "#nitro";
+// Cannot find module '#imports' or its corresponding type declarations.ts(2307)
+import { defineNitroPlugin } from "#imports";
 
 export default defineNitroPlugin((nitroApp: NitroApp) => {
   // Argument of type '"render:html"' is not assignable to parameter of type 'HookKeys<NitroRuntimeHooks>'.ts(2345)
